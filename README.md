297c5e9dec0363afb0fed58b91c50189230eca8d
devcamila <camilacerda3@gmail.com>
2021-09-30 19:17:05 -0300

    add files

---
+++ README.md
@@ -0,0 +1,25 @@
+### Super Trunfo - Fatos Históricos
+Site construído durante a Imersão Dev da Alura <br>
+
+--- 
+<div align="center">
+<strong>Acesse o projeto aqui</strong> ➡️ https://mentalista-imersao-dev.vercel.app/
+</div>
+
+---
+[![Tp1FfG.png](https://i.im.ge/2021/10/01/Tp1FfG.png)](https://im.ge/i/Tp1FfG)
+
+---
+Feito com amor :hugs: por <font color="yellow"> **Dev Camila Lacerda**</font>    | [*Conecte-se ao meu LinkedIn*](https://www.linkedin.com/in/camila-lacerda/)
+
+https://i.im.ge/2021/09/24/Tbi0Sa.png
+
+
+
+https://i.im.ge/2021/09/24/Tbic5D.png
+https://i.im.ge/2021/09/24/TbixNh.png
+https://i.im.ge/2021/09/24/TbiES8.png
+https://i.im.ge/2021/09/24/TbibrY.png
+https://i.im.ge/2021/09/24/Tbi3fM.png
+https://i.im.ge/2021/09/24/Tbigg4.png
+https://i.im.ge/2021/09/24/TbiRMC.png
---
+++ index.js
@@ -0,0 +1,342 @@
+//* Cartas
+let homemLua = {
+  nome: "Chegada do homem à Lua",
+  imagem:
+    "https://revista-images.querobolsa.com.br/revista/post_images/17913/87da5c42a68b7a389187b45f33f6a10a48d31715.png?1554854234",
+  atributos: {
+    Impacto: 8,
+    Ano: 1969,
+    Força: 7
+  }
+};
+
+let adaLovelace = {
+  nome: "Ada Lovelace: 1ª programadora ",
+  imagem:
+    "http://www.anarquista.net/wp-content/uploads/2018/03/ada-lovelace-primeira-programadora-da-historia.jpg",
+  atributos: {
+    Impacto: 10,
+    Ano: 1842,
+    Força: 4
+  }
+};
+
+let abolicaoEscravatura = {
+  nome: "Abolição da Escravatura",
+  imagem:
+    "https://conhecimentocientifico.r7.com/wp-content/uploads/2020/03/abraham-lincoln-abolio.jpg",
+  atributos: {
+    Impacto: 6,
+    Ano: 1842,
+    Força: 9
+  }
+};
+
+let fimMonarquia = {
+  nome: "Inicio da República no Brasil",
+  imagem:
+    "https://2.bp.blogspot.com/-kTMiKGWpOuQ/WQicqEjIXzI/AAAAAAAAAJ0/BmS4WTcv4RAHEFk0yWNlOXPd0HoUFfpVQCLcB/s400/foto%2B1.jpg",
+  atributos: {
+    Impacto: 6,
+    Ano: 1889,
+    Força: 4
+  }
+};
+
+let revIndustriais = {
+  nome: "Primeira Revolução Industrial",
+  imagem:
+    "https://www.significados.com.br/foto/revolucao-industrial-1.jpg",
+  atributos: {
+    Impacto: 5,
+    Ano: 1879,
+    Força: 8
+  }
+};
+
+let revIndustriais3 = {
+  nome: "Terceira Revolução Industrial",
+  imagem:
+    "https://www.infoescola.com/wp-content/uploads/2021/04/terceira-revolucao-industrial-automacao-733812256.jpg",
+  atributos: {
+    Impacto: 10,
+    Ano: 1950,
+    Força: 3
+  }
+};
+
+let segundaGuerra = {
+  nome: "Segunda Guerra Mundial",
+  imagem:
+    "https://ichef.bbci.co.uk/news/640/cpsprodpb/153D9/production/_108310078_1.jpg",
+  atributos: {
+    Impacto: 4,
+    Ano: 1939,
+    Força: 10
+  }
+};
+
+let guerraFria = {
+  nome: "Fim da Guerra Fria",
+  imagem:
+    "https://guiadoensino.com.br/wp-content/uploads/2020/01/guerra-fria-noticias-ao-minuto-758x426.jpg",
+  atributos: {
+    Impacto: 6,
+    Ano: 1989,
+    Força: 2
+  }
+};
+
+let quedaRoma = {
+  nome: "Queda Império Romano",
+  imagem:
+    "https://s1.static.brasilescola.uol.com.br/be/conteudo/images/ruinas-forum-romano-na-cidade-roma-italia-58f62367e6c67.jpg",
+  atributos: {
+    Impacto: 8,
+    Ano: 476,
+    Força: 7
+  }
+};
+
+let votoFeminino = {
+  nome: "Voto Feminino no Brasil",
+  imagem:
+    "http://www.ugt.org.br/upload/img2-Conquista-do-voto-feminino-no-Brasil-com-18748.jpg",
+  atributos: {
+    Impacto: 10,
+    Ano: 1932,
+    Força: 3
+  }
+};
+
+let piramideEgito = {
+  nome: "Construção pirâmides Egito",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/All_Gizah_Pyramids.jpg/330px-All_Gizah_Pyramids.jpg",
+  atributos: {
+    Impacto: 4,
+    Ano: -2700,
+    Força: 8
+  }
+};
+
+let voltaMundo = {
+  nome: "Primeira volta ao mundo",
+  imagem:
+    "https://i1.wp.com/passageirodeprimeira.com/wp-content/uploads/2019/08/volta-ao-mundo.png?fit=1200%2C627&ssl=1",
+  atributos: {
+    Impacto: 1,
+    Ano: 1521,
+    Força: 4
+  }
+};
+
+let cherno = {
+  nome: "Acidente nuclear de Chernobyl",
+  imagem:
+    "https://revista-images.querobolsa.com.br/revista/post_images/34234/2fb38c8f2e7a90b313d3a3452e5cfce2617ef162.jpg?1612306152",
+  atributos: {
+    Impacto: 7,
+    Ano: 1986,
+    Força: 3
+  }
+};
+
+let onzeSetembro = {
+  nome: "Atentado 11 de Setembro",
+  imagem:
+    "https://i0.wp.com/jornal.usp.br/wp-content/uploads/2021/09/20210909_11desetembro.png?fit=1200%2C630&ssl=1",
+  atributos: {
+    Impacto: 6,
+    Ano: 2001,
+    Força: 8
+  }
+};
+
+let quartozeBis = {
+  nome: "1º voo do avião 14-bis",
+  imagem:
+    "https://revista-images.querobolsa.com.br/revista/post_images/34230/f14972e98c672560f432706887b49e2f830052cb.jpg?1612305532",
+  atributos: {
+    Impacto: 9,
+    Ano: 2001,
+    Força: 4
+  }
+};
+
+let primavera = {
+  nome: "Primavera Árabe",
+  imagem:
+    "https://revista-images.querobolsa.com.br/revista/post_images/34235/3e4e2070601677d90c50230efcc7e4b66a93b3e5.png?1612306311",
+  atributos: {
+    Impacto: 4,
+    Ano: 2011,
+    Força: 10
+  }
+};
+
+let incMineira = {
+  nome: "Inconfidência Mineira",
+  imagem:
+    "https://super.abril.com.br/wp-content/uploads/2020/04/si_bandeira_mineira_fb.jpg?quality=70&strip=info",
+  atributos: {
+    Impacto: 7,
+    Ano: 1789,
+    Força: 2
+  }
+};
+
+let canudos = {
+  nome: "Guerra de Canudos ",
+  imagem:
+    "https://www.todoestudo.com.br/wp-content/uploads/2018/09/guerra-de-canudos.jpg",
+  atributos: {
+    Impacto: 5,
+    Ano: 1896,
+    Força: 3
+  }
+};
+
+let revFrancesa = {
+  nome: "Revolução Francesa",
+  imagem:
+    "https://lh3.googleusercontent.com/proxy/H5bqNiSxIXvPSR42m7OBA69cleDIyqXhzi6ZROAeURX_i2mHuSATrM1dXtJ9NGXOXvaxjtGJzf4jmMIXLk77vZKN09uX7L_-F7417LPjmJLepnu-XE3-fuBnwi99RZT0E9xUyWMgYMfuKg",
+  atributos: {
+    Impacto: 9,
+    Ano: 2001,
+    Força: 4
+  }
+};
+
+let fimDitadura = {
+  nome: "Fim ditadura Militar",
+  imagem:
+    "https://revista-images.querobolsa.com.br/revista/post_images/11844/0780b8144d0ad4f507e89b80cc7edbb48bbefacf.png?1540840893",
+  atributos: {
+    Impacto: 7,
+    Ano: 1985,
+    Força: 9
+  }
+};
+
+let crise29 = {
+  nome: "Crise de 1929",
+  imagem:
+    "https://www.al.sp.gov.br/repositorio/noticia/hist/wallstreet%201929.jpg",
+  atributos: {
+    Impacto: 4,
+    Ano: 2001,
+    Força: 1
+  }
+};
+
+
+
+let cartaMaquina;
+let cartaJogador;
+let cartas = [homemLua, adaLovelace, abolicaoEscravatura, fimMonarquia, revIndustriais, revIndustriais3, segundaGuerra, guerraFria, quedaRoma, votoFeminino, piramideEgito, voltaMundo, cherno, onzeSetembro, quartozeBis, primavera, incMineira, canudos, revFrancesa, fimDitadura, crise29];
+
+document.getElementById("btnJogar").style.display = "none";
+document.getElementById("novo-jogo").style.display = "none";
+document.getElementById("atributo").style.display = "none";
+
+function sortearCarta() {
+  let numeroCartaMaquina = parseInt(Math.random() * 21);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  let numeroCartaJogador = parseInt(Math.random() * 21);
+  while (numeroCartaJogador == numeroCartaMaquina) {
+    numeroCartaJogador = parseInt(Math.random() * 21);
+  }
+  cartaJogador = cartas[numeroCartaJogador];
+  console.log(cartaJogador);
+
+  document.getElementById("atributo").style.display = "flex";
+  document.getElementById("btnJogar").style.display = "flex"; 
+  document.getElementById("btnJogar").disabled = false;
+  document.getElementById("btnSortear").style.display = "none";
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  let radioAtributo = document.getElementsByName("atributo");
+  for (let i = 0; i < radioAtributo.length; i++) {
+    if (radioAtributo[i].checked) {
+      return radioAtributo[i].value;
+    }
+  }
+}
+
+function jogar() {
+  let atributoSelecionado = obtemAtributoSelecionado();
+  let divResultado = document.getElementById("resultado");
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>Venceu</p>";
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>Perdeu</p>";
+  } else {
+    htmlResultado = "<p class='resultado-final'>Empatou</p>";
+  }
+  divResultado.innerHTML = htmlResultado;
+
+  document.getElementById("atributo").style.display = "none";
+  document.getElementById("btnJogar").style.display = "none";  
+  document.getElementById("novo-jogo").style.display = "flex";
+  exibirCartaMaquina();
+}
+
+function exibirCartaJogador() {
+  let divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  let moldura =
+    '<img src="https://i.im.ge/2021/09/30/TpX09S.png" style=" width: inherit; height: inherit; position: absolute;">';
+  let tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  let opcoesTexto = "";
+  for (let atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='checkbox' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>" 
+      ;
+  }
+  
+  let nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+  let divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  let moldura =
+    '<img src="https://i.im.ge/2021/09/30/TpX09S.png"  style=" width: inherit; height: inherit; position: absolute;">';
+  let tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  let opcoesTexto = "";
+  for (let atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  let nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
